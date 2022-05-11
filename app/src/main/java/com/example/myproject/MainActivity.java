01bc4d5b5b86e5bd38e13c0b4b3057e0e3b2355b
Christoffer <a18chrkr@gmail.com>
2022-05-11 15:09:05 +0200

    Skapat medlemsvariabler samt kopplat variabeln med datatypen recyclerview till ID:et för recyclerviewen i aktiviteten. Givit den en Linear Layout och kopplat adaptern till den.

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a18chrkr";
     private final String TAG = "==>";
+    private List<Wonder> listOfWonders;
+    private WonderAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listOfWonders = new ArrayList<Wonder>();
+        adapter = new WonderAdapter();
+
+        recyclerView = findViewById(R.id.wonder_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 }
