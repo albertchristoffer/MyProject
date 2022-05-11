8652a1f3f2eb790e28671ca0da9c4cb1a41e2531
Christoffer <a18chrkr@gmail.com>
2022-05-11 14:51:56 +0200

    Skapade nya klasser för mitt wonderobjekt, min adapter och en viewholder. Sparade länken för JSON-datan i en variabel.

--- app/src/main/java/com/example/myproject/Wonder.java
+++ app/src/main/java/com/example/myproject/Wonder.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a18chrkr";
-    private final String TAG = "==>";
+public class Wonder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wonder);
     }
 }
