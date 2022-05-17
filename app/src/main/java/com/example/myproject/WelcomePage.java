9a360f9c46efbe39bb5376dbbce28ecec428c197
Christoffer <a18chrkr@gmail.com>
2022-05-17 16:46:02 +0200

    Designade klart startsidan. Skapade en knapp som tar användaren från welcome_page till MainActivity.

--- app/src/main/java/com/example/myproject/WelcomePage.java
+++ app/src/main/java/com/example/myproject/WelcomePage.java
@@ -2,13 +2,29 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomePage extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
+        button = findViewById(R.id.explore_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(WelcomePage.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
