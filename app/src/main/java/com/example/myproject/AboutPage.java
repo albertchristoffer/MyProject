53b134090784808b8863074745ac95036931a236
Christoffer <a18chrkr@gmail.com>
2022-05-23 14:48:09 +0200

    Skapade en stycke i about-sidan om copyright. La till textviews som hämtade strängar från strings.xml. I dessa strängar la jag till hyperlänkar som tar användaren till webbsidor med hjälpa av setMovementMethod-metoder i AboutPage.java.

--- app/src/main/java/com/example/myproject/AboutPage.java
+++ app/src/main/java/com/example/myproject/AboutPage.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutPage extends AppCompatActivity {
 
     private Button button;
+    private TextView wikipedia;
+    private TextView unsplash;
+    private TextView copyright;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,13 @@ public class AboutPage extends AppCompatActivity {
         setContentView(R.layout.about_page);
 
         button = findViewById(R.id.back_button);
+        wikipedia = findViewById(R.id.wikipedia);
+        unsplash = findViewById(R.id.unsplash);
+        copyright = findViewById(R.id.copyright);
+
+        wikipedia.setMovementMethod(LinkMovementMethod.getInstance());
+        unsplash.setMovementMethod(LinkMovementMethod.getInstance());
+        copyright.setMovementMethod(LinkMovementMethod.getInstance());
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
